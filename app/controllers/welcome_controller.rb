561dc5203c7178bda2bca1a50a99b73d91e8d887
Dylan Ham <dylanham@gmail.com>
2015-01-23 11:10:34 -0700

    add second page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,7 @@ class WelcomeController < ApplicationController
   def index
   end
 
+  def page2
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   root 'welcome#index'
+  get  "/page2/",'welcome#page2'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
